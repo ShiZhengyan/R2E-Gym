9cfefb590ff99c6c64ed15c05e5507f80f83c5e5
ZhengyanShi <michaelszx117@gmail.com>
2025-08-24 12:54:31 -0400

    update script

--- run_agent/run_1r1m.sh
+++ run_agent/run_1r1m.sh
@@ -8,20 +8,23 @@ echo "Starting SWE-Bench evaluation with R2EGym-32B-Agent..."
 # Configuration
 MODEL_NAME="capi-claude-sonnet-4"
 # MODEL_NAME="capi-o3-2025-04-16"
-# DATASET="R2E-Gym/R2E-Gym-Subset"
 DATASET="/home/zhengyanshi/project/SWE-smith/logs/automated_pipeline_o3_bugs30_combos50_depth2_workers32_nbugs1_patches2_perfile2_permodule10/astropy__astropy.26d14786/task_insts/astropy__astropy.26d14786_ps.json"
 # DATASET_SLUG=$(echo "$DATASET" | sed 's|/|--|g')
+K=2313
+K=10
 DATASET_SLUG=astropy__astropy.26d14786_ps
 SPLIT="train"
-MAX_WORKERS=96
+MAX_WORKERS=64
 START_IDX=0
 MAX_STEPS=40
 MAX_STEPS_ABS=50
 TEMPERATURE=1.0
 K_RESPONSES=5
-BACKEND="kubernetes"
+# BACKEND="kubernetes"
+BACKEND="docker"
 API_ENDPOINT="not-needed"  # vLLM server endpoint
-EXP_NAME="${DATASET_SLUG}-${MODEL_NAME}-${SPLIT}-s${MAX_STEPS}-${MAX_STEPS_ABS}-t${TEMPERATURE}-k${K_RESPONSES}-${BACKEND}"
+EXP_NAME="test_${DATASET_SLUG}-${MODEL_NAME}-${SPLIT}-s${MAX_STEPS}-${MAX_STEPS_ABS}-t${TEMPERATURE}-k${K_RESPONSES}-${BACKEND}-${MAX_WORKERS}"
+# EXP_NAME="${DATASET_SLUG}-${MODEL_NAME}-${SPLIT}-s${MAX_STEPS}-${MAX_STEPS_ABS}-t${TEMPERATURE}-k${K_RESPONSES}-${BACKEND}-${MAX_WORKERS}"
 TRAJ_DIR="./traj"
 export OPENAI_API_KEY="not-needed"
 
@@ -51,11 +54,12 @@ source .venv/bin/activate
 uv run python src/r2egym/agenthub/run/edit.py runagent_multiple \
   --dataset "${DATASET}" \
   --split "${SPLIT}" \
+  --k "${K}" \
   --traj_dir "${TRAJ_DIR}" \
   --exp_name "${EXP_NAME}" \
   --start_idx "${START_IDX}" \
   --max_steps "${MAX_STEPS}" \
-  --max_steps_absolute "${MAX_STEPS}" \
+  --max_steps_absolute "${MAX_STEPS_ABS}" \
   --max_workers "${MAX_WORKERS}" \
   --llm_name "${MODEL_NAME}" \
   --k_responses "${K_RESPONSES}" \
